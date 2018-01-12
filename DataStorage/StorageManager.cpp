81ae13ac353813d098e1e6f1e1c8ee61cf093342
sb <caot.50f14n@hotmail.com>
2018-01-12 01:28:58 +0100

    started p5 1

--- DataStorage/StorageManager.cpp
+++ DataStorage/StorageManager.cpp
@@ -6,7 +6,8 @@ StorageManager::StorageManager() {
 
 StorageManager::StorageManager(std::string homepath) : homePath(homepath) {
 };
-StorageManager::StorageManager(std::string homepath, int buffersize) 
+
+StorageManager::StorageManager(std::string homepath, int buffersize)
 : homePath(homepath), bufferSize(buffersize) {
 };
 
@@ -16,25 +17,27 @@ StorageManager::StorageManager(const StorageManager& orig) {
 StorageManager::~StorageManager() {
 }
 
-int StorageManager::newId() {
+int StorageManager::newBlockID() {
     blockIdCtr++;
     return blockIdCtr;
 }
 
+
+
 void StorageManager::init_store(int total_blocks, int ram_blocks) {
 
     //Abort possibilities 
-    if (total_blocks < ram_blocks){
-        std::cout<<"init_store: more Ram than TotalBlocks";
+    if (total_blocks < ram_blocks) {
+        std::cout << "init_store: more Ram than TotalBlocks";
         return;
     }
-    
+
     buffer.resize(bufferSize, ' ');
     //First create Blocks on Storage
     for (int i = 0; i < total_blocks; i++) {
-        int id = newId();
+        int id = newBlockID();
         writeS.open(homePath + std::to_string(id));
-       // writeS << buffer; versteh ich nicht
+        // writeS << buffer; versteh ich nicht
         writeS.close();
         totalBlocks.push_back(id);
     }
@@ -43,15 +46,40 @@ void StorageManager::init_store(int total_blocks, int ram_blocks) {
     //Already used
     for (int j = 0; j < ram_blocks; j++) {
         int id = totalBlocks.at(total_blocks - j);
-        Block newy; 
+        Block newy;
         newy.id = id;
         readS.open(homePath + std::to_string(id));
-        readS>>newy.data;
+        readS >> newy.data;
         readS.close();
         holdedBlocks.insert(std::pair<Block, bool> (newy, true));
     }
     return;
 }
+
+void StorageManager::cleanup_store() {
+    while (blockIdCtr > 0) {
+        std::remove((homePath + std::to_string(blockIdCtr)).c_str());
+        blockIdCtr--;
+    }
+    totalBlocks.clear();
+    holdedBlocks.clear();
+    buffer.resize(bufferSize, ' ');
+
+}
+
+int StorageManager::readCacheBlock(int blockID, std::string& buffer) {
+
+    auto it = holdedBlocks.begin();
+    for (it; it  != holdedBlocks.end(); it++){
+        if ((*it).first.id == blockID){
+            this->buffer = (*it).first.data;
+            *it.first.lastused = time(0);
+        }
+           
+}
+}
+
+
 
 
 
--- DataStorage/StorageManager.h
+++ DataStorage/StorageManager.h
@@ -7,44 +7,49 @@
 #include <vector>
 #include <map>
 
+#include <sys/stat.h>
 
-struct Block{
-    
-    int id; 
+struct Block {
+    int id;
     std::string data;
     time_t fetched = time(0);
     time_t lastused = time(0);
+
+    bool operator<(const Block& right) const {
+        return this->lastused < right.lastused;
+    }
+
 };
 
-class StorageManager 
-{
+class StorageManager {
 public:
-    
+
     StorageManager();
     StorageManager(std::string homepath);
     StorageManager(std::string homepath, int buffersize);
     StorageManager(const StorageManager& orig);
     virtual ~StorageManager();
-    
+
     void init_store(int total_blocks, int ram_blocks);
     void cleanup_store();
     int readCacheBlock(int blockID, std::string &buffer);
     int writeCacheBlock(int BlockID, std::string buffer);
     int freeCacheBlock(int blockID);
-    
+
+    std::string buffer;
+
 private:
 
-    int newId();
+    bool blockExists(const int id);
+    int newBlockID();
     int blockIdCtr = 0;
-    int bufferSize; 
+    int bufferSize;
     std::string homePath;
-    std::string buffer; 
-    std::vector<int> totalBlocks;
-    std::map<Block, bool> holdedBlocks;     //bool: consistent y/n
-    
     std::ifstream readS;
     std::ofstream writeS;
-    
+    std::vector<int> totalBlocks;
+    std::map<Block, int> holdedBlocks; //bool: consistent y/n
+
 };
 
 #endif /* STORAGEMANAGER_H */
